1fbcae2c6adba0ce57ac5176048ab0be80bfb095
Kristen Chellis <peanutster@gmail.com>
2018-03-23 11:41:48 -0700

    Add CSS styling and alert if name is not entered

--- js/scripts.js
+++ js/scripts.js
@@ -22,23 +22,28 @@ var maxLetter= function(string) {
 $(function() {
   $("form#choose").submit(function(event) {
     event.preventDefault();
-    $(".options").hide();
-    $("span").text($("#name").val());
-    var ans1 = $("#firstQ").val();
-    var ans2 = $("#secondQ").val();
-    var ans3 = $("#thirdQ").val();
-    var ans4 = $("#fourthQ").val();
-    var ans5 = $("#fifthQ").val();
-    if (ans1 === "f") {
-      $("#css").slideDown();
-    }else if (maxLetter(answers(ans2, ans3, ans4, ans5)) === "j") {
-      $("#java").slideDown();
-    }else if (maxLetter(answers(ans2, ans3, ans4, ans5)) === "r") {
-      $("#ruby").slideDown();
-    }else if (maxLetter(answers(ans2, ans3, ans4, ans5)) === "c") {
-      $("#csharp").slideDown();
-    }else {
-      $("#none").slideDown();
+    var name = $("#name").val();
+    if (name) {
+      $(".options").hide();
+      $("span").text(name);
+      var ans1 = $("#firstQ").val();
+      var ans2 = $("#secondQ").val();
+      var ans3 = $("#thirdQ").val();
+      var ans4 = $("#fourthQ").val();
+      var ans5 = $("#fifthQ").val();
+      if (ans1 === "f") {
+        $("#css").slideDown();
+      }else if (maxLetter(answers(ans2, ans3, ans4, ans5)) === "j") {
+        $("#java").slideDown();
+      }else if (maxLetter(answers(ans2, ans3, ans4, ans5)) === "r") {
+        $("#ruby").slideDown();
+      }else if (maxLetter(answers(ans2, ans3, ans4, ans5)) === "c") {
+        $("#csharp").slideDown();
+      }else {
+        $("#none").slideDown();
+      }
+    } else {
+      alert("Please enter a name!");
     }
   });
 });
