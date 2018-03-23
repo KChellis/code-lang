120f87fb916205714c6143e68aa04ee2f4f4fd60
Kristen Chellis <peanutster@gmail.com>
2018-03-23 11:07:19 -0700

    Finalize and debug javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
 var answers = function(string1, string2, string3, string4) {
   return string1 + string2 + string3 + string4;
 }
-
+var maxLetter= function(string) {
+  var exp = string ;
+  var expCounts = {};
+  var maxKey = '';
+  for(var i = 0; i < exp.length; i++)
+  {
+      var key = exp[i];
+      if(!expCounts[key]){
+       expCounts[key] = 0;
+      }
+      expCounts[key]++;
+      if(maxKey == '' || expCounts[key] > expCounts[maxKey]){
+          maxKey = key;
+      }
+  }
+  return maxKey;
+}
 
 $(function() {
   $("form#choose").submit(function(event) {
     event.preventDefault();
     $(".options").hide();
-    ("span.user").text("#name");
+    $("span").text($("#name").val());
     var ans1 = $("#firstQ").val();
     var ans2 = $("#secondQ").val();
     var ans3 = $("#thirdQ").val();
     var ans4 = $("#fourthQ").val();
     var ans5 = $("#fifthQ").val();
-
     if (ans1 === "f") {
-      ("#css").slideDown();
+      $("#css").slideDown();
     }else if (maxLetter(answers(ans2, ans3, ans4, ans5)) === "j") {
       $("#java").slideDown();
     }else if (maxLetter(answers(ans2, ans3, ans4, ans5)) === "r") {
